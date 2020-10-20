af1b292e15e2f4d49bce65dd4a175a446176f8ab
Persei08 <Persei08@users.noreply.github.com>
2020-10-20 15:28:25 +0200

    Fixing some compatibility issues Should works with 0.3.8-5, 0.3.9, 0.3.10, 0.4.4. Still bugs and some issues are on PCT side too.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready( function() {
     var bt = butter_remote;
 
     function getList() {
-        bt.getcurrenttab(function(data) { 
+        bt.getcurrenttab(function(data) {
             tab = data.result.tab;
             //Delete the actual active tab
             $("#tabs a, #right i").removeClass("active");
@@ -21,13 +21,28 @@ $(document).ready( function() {
             //Repeats the function until butter loaded the items so that can respond correctly
             var interval = setInterval(function() {
                 bt.getcurrentlist(function(data) {
+                    console.info('getcurrentlist');
                     //if errors are not returned
                     if(!data.error) {
                         var htmlList = "";
                         //Cycle the elements of the list
                         $.each(data.result.list, function(index, item) {
-                            //Given the differences between the movies and the shows/anime object, they have to be filtered differently
-                            if(this.type=="movie" || this.type=="bookmarkedmovie") { cover=this.image; } else if(this.type=="show" || this.type=="bookmarkedshow") { cover=this.images.poster; }
+                            cover = 'assets/img/posterholder.png'; // default cover
+                            //Given the differences between the movies and the shows/anime object,
+                            // they have to be filtered differently
+                            switch (this.type) {
+                            case 'movie':
+                            case 'bookmarkedmovie':
+                                cover = this.image;
+                                break;
+                            case 'show':
+                            case 'bookmarkedshow':
+                                cover = this.images === undefined ? this.poster : this.images.poster;
+                                break;
+                            default:
+                                console.error('Error getcurrentlist: this.type value unexpected: ', this );
+                                break;
+                            }
                             //Build the html
                             htmlList += '<li><a class="'+index+'" id="open-item"><div style="width:134px; height:201px; background-color:#000;background-size:cover; background-image:url('+cover+');"></div><p>'+this.title+'</p><p style="color:#5b5b5b; font-size:0.75em;">'+this.year+'</p></a></li>';
                         });
@@ -38,7 +53,7 @@ $(document).ready( function() {
                     }
 
                 });
-            }, 100);
+            }, 1000);
         });
     }
 
@@ -46,25 +61,34 @@ $(document).ready( function() {
         //Empty the item's list
         bt.getselection(function(data) {
             var cover, genres = '', playButtons = '';
-            if(data.result.type=="movie" || data.result.type=="bookmarkedmovie") { 
-                cover = data.result.cover;
+            var result = data.result;
+            switch (result.type) {
+            case 'movie':
+            case 'bookmarkedmovie':
+                cover = result.cover;
                 playButtons = '<a id="watch-now">Watch Now</a> <a id="watch-trailer">Watch Trailer</a> <a id="toggle-quality"></a>';
 
-                data.result.genre.forEach(function(item, i) {
-                    trailingSlash = (i != data.result.genre.length-1 ? "/" : "");
+                result.genre.forEach(function(item, i) {
+                    trailingSlash = (i != result.genre.length-1 ? "/" : "");
                     genres += item+trailingSlash;
                 });
-            } else if(data.result.type=="show" || data.result.type=="bookmarkedshow") {
-                cover = data.result.images.poster;
+                break;
+            case 'show':
+            case 'bookmarkedshow':
+            default:
+                if (!result.type=="show" || !result.type=="bookmarkedshow"){
+                    console.warn('result.type = %s ... let\'s assume it\' as show...', result.type);
+                }
+                cover = result.images === undefined ? result.poster : result.images.poster;
                 playButtons = '<a id="watch-now">Watch Now</a>';
 
-                data.result.genres.forEach(function(item, i) {
-                    trailingSlash = (i != data.result.genres.length-1 ? "/" : "");
+                result.genres.forEach(function(item, i) {
+                    trailingSlash = (i != result.genres.length-1 ? "/" : "");
                     genres += item+trailingSlash;
                 });
+                break;
             }
-            console.log(data.result);
-            var htmlDetail = '<section id="item-detail"><div id="item-image"><img src="'+cover+'" width="500" /></div> <div id="item-descr"><p id="title">'+data.result.title+'</p><p style="color:#5b5b5b; font-size:0.75em;">'+data.result.year+' | '+data.result.runtime+'min. | '+genres+' | IMDB: '+data.result.rating+'</p><p>'+data.result.synopsis+'</p> '+playButtons+' <i id="toggle-favourite" class="fa fa-heart"></i> <i id="toggle-watched"class="fa fa-eye-slash"></i> </div><div style="clear:both;"></div></section>';
+            var htmlDetail = '<section id="item-detail"><div id="item-image"><img src="'+cover+'" width="350"/></div> <div id="item-descr"><p id="title">'+result.title+'</p><p class="metadatas"">'+result.year+' | '+result.runtime+'min. | '+genres+' | IMDB: '+result.rating+'</p><p class="synopsis">'+result.synopsis+'</p> '+playButtons+' <i id="toggle-favourite" class="fa fa-heart"></i> <i id="toggle-watched"class="fa fa-eye-slash"></i> </div><div style="clear:both;"></div></section>';
             $("#container").html(htmlDetail);
 
         });
@@ -95,14 +119,19 @@ $(document).ready( function() {
             } else if(view == "movie-detail" || view == "shows-container-contain") {
                 itemDetails();
             }
-            
+
         });
 
-        //Lets repeat operations every second
+        //Lets repeat operations every n miliseconds
         setInterval(function() {
             //Listen for notifications
+            //console.warn('listennotifications');
             bt.listennotifications(function(data) {
+                if (data.result) {
+                    console.warn('data.result', data.result);
+                }
                 if(data.result.events.viewstack) {
+                    console.warn('viewstack');
                     view = data.result.events.viewstack[data.result.events.viewstack.length-1];
                     if(view == "main-browser" || view == "init-container") {
                         getList();
@@ -111,7 +140,7 @@ $(document).ready( function() {
                     }
 
                 }
-                
+
             });
 
         }, 1000);
@@ -126,20 +155,29 @@ $(document).ready( function() {
         bt.enter();
     });
 
-    //Click on a tab 
+    //Click on a tab
     $("#tabs > a, #right > i").click(function() {
         id = $(this).attr("id");
         //Switch to the clicked tab
-        if(id == "movies") { 
-            bt.movieslist(); 
-        } else if(id == "shows") {
-            bt.showslist(); 
-        } else if(id == "anime") { 
-            bt.animelist(); 
-        } else if(id == "Favorites") { 
-            bt.showfavourites(); 
-        } else if(id == "Watchlist") { 
-            bt.showwatchlist(); 
+        switch (id) {
+            case 'movies':
+                bt.movieslist();
+                break;
+            case 'shows':
+                bt.showslist();
+                break;
+            case 'anime':
+                bt.animelist();
+                break;
+            case 'Favorites':
+                bt.showfavourites();
+                break;
+            case 'Watchlist':
+                bt.showwatchlist();
+                break;
+            default:
+                console.error('Error: unhandled clicked tab id = ', id);
+                break;
         }
     });
 
